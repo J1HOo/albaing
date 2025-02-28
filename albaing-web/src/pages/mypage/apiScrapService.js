74092e619b404a83113478434752d2c188c9b383
coiilee <dakyung96@gmail.com>
2025-02-28 17:29:32 +0900

    apiScrapService

---
+++ albaing-web/src/pages/mypage/apiScrapService.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+
+const API_MYPAGE_URL = "http://localhost.8080/api/scrap";
+
+const apiScrapService = {
+
+    insertScrap : function (){
+        axios.get()
+            .then(
+
+            )
+            .catch()
+    },
+
+    deleteScrap : function (){
+
+    },
+
+    getScrapsByUser : function (){
+
+    },
+
+
+
+}
+export default apiScrapService;
