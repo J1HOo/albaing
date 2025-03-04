43ac6c10dc073c4c46386f0dcac2af96b176f0a8
coiilee <dakyung96@gmail.com>
2025-03-04 16:15:47 +0900

    front

--- albaing-web/src/pages/resume/apiResumeService.js
+++ albaing-web/src/pages/resume/apiResumeService.js
@@ -1,55 +1,24 @@
 import axios from "axios";
 
-// 학교 API URL 목록
-const API_URLS = {
-    middle: "https://www.career.go.kr/cnet/openapi/getOpenApi?apiKey=cdd817e14883a15964aff585352a4b8f&svcType=api&svcCode=SCHOOL&contentType=json&gubun=midd_list&thisPage=1&perPage=1000000",
-    high: "https://www.career.go.kr/cnet/openapi/getOpenApi?apiKey=cdd817e14883a15964aff585352a4b8f&svcType=api&svcCode=SCHOOL&contentType=json&gubun=high_list&thisPage=1&perPage=1000000",
-    university: "https://www.career.go.kr/cnet/openapi/getOpenApi?apiKey=cdd817e14883a15964aff585352a4b8f&svcType=api&svcCode=SCHOOL&contentType=json&gubun=univ_list&thisPage=1&perPage=1000000",
-};
-
-// 전공 API URL 목록
-const MAJOR_API_URLS = {
-    high: "https://www.career.go.kr/cnet/openapi/getOpenApi?apiKey=cdd817e14883a15964aff585352a4b8f&svcType=api&svcCode=MAJOR&contentType=json&gubun=high_list",
-    university: "https://www.career.go.kr/cnet/openapi/getOpenApi?apiKey=cdd817e14883a15964aff585352a4b8f&svcType=api&svcCode=MAJOR&contentType=json&gubun=univ_list",
-};
-
-// API 호출 함수
-const fetchData = (url) => {
-    return axios
-        .get(url)
-        .then((response) => {
-            return response.data.dataSearch?.content || [];
-        })
-        .catch((error) => {
-            console.error("API 호출 오류:", error);
-            return [];
-        });
-};
-
-// 학교 목록 가져오기
-export const getAllSchools = async () => {
-    const middleSchools = await fetchData(API_URLS.middle);
-    const highSchools = await fetchData(API_URLS.high);
-    const universities = await fetchData(API_URLS.university);
-
-    return [
-        ...middleSchools.map((school) => ({ name: school.schoolName, type: "중학교" })),
-        ...highSchools.map((school) => ({ name: school.schoolName, type: "고등학교" })),
-        ...universities.map((school) => ({ name: school.schoolName, type: "대학교" })),
-    ];
-};
-
-// 전공 목록 가져오기
-export const getAllMajors = async (schoolType) => {
-    const url = MAJOR_API_URLS[schoolType];
-    if (!url) return [];
-
-    return await fetchData(url);
-};
 
 const API_RESUME_URL = "http://localhost:8080/api/resume";
 
 const apiResumeService = {
+
+    //user 정보 get 해오기
+    getUserInfo : function (setUser){
+        axios.get("http://localhost:8080/api/resume")
+            .then(
+                (res)=> {
+                    setUser(res.data)
+                    console.log("백엔드 연결 성공");
+                }
+            )
+            .catch((err)=>{
+                alert("정보를 불러오는데 에러가 발생했습니다.");
+                console.error("user정보 불러오기 에러 : ",err);
+            })
+    },
 
     //이력서 생성
     createResumeForUser: function (postResume,callback){
